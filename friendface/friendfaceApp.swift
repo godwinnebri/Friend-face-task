120b929d536bad498761086548da819230a90330
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2024-02-20 15:55:10 +0100

    Initial Commit

---
+++ friendface/friendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendfaceApp.swift
+//  friendface
+//
+//  Created by Godwin IE on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct friendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
